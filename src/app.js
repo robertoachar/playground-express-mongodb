a9f1624b21c97bdad3b8e85235eacfa88c0e581d
Roberto Achar <robertoachar@gmail.com>
2018-03-31 22:15:41 -0300

    fix: append api to user routes

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use('/', router);
-app.use('/users', userRouter);
+app.use('/api/users', userRouter);
 
 app.use(notFound);
 app.use(catchAll);
